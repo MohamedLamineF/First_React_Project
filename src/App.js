3f3a8233a243710f83e6af1e7987702483e7e6bb
Mohamed Lamine <fadimoh@hotmail.fr>
2021-08-16 14:00:44 +0100

    first_commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+import "./App.css";
+
+const App = () => {
+  const [hex, setHex] = useState("#aaaaaa");
+  const [btn, setbtn] = useState("#ff0000");
+  
+  var secondColor = () => {
+    secondColor = "#aaaaaa";
+    if (hex !== "#ff0000") {
+      secondColor = "#ff0000";
+    }
+    
+    let secondColor2 = "#ff0000";
+    if (btn !== "#aaaaaa") {
+      secondColor2 = "#aaaaaa";
+    }
+    
+    setHex(secondColor);
+    setbtn(secondColor2);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ backgroundColor: `${hex}`, height: "100%" }}>
+      <h2 className="colorName">{hex}</h2>
+      <button onClick={secondColor} className="button" style={{backgroundColor: `${btn}`}}>
+        CHANGE COLOR
+      </button>
     </div>
   );
-}
+};
 
 export default App;
